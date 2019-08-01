8ab4d3840a734da5a89fcd8cc0a1f4c1d562d0ac
Aonthemove <ambergipson86@gmail.com>
2019-08-01 10:17:02 -0500

    first commit

---
+++ californiaCustom/CaliforniaCustomApplication.java
@@ -0,0 +1,13 @@
+package com.example.californiaCustom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaliforniaCustomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaliforniaCustomApplication.class, args);
+	}
+
+}
